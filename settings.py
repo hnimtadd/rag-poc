30b35af4b15fa54ee410560bb67056fd0af06e26
Dat Nguyen <minhdat15012002@gmail.com>
2024-06-09 18:20:06 +0700

    feat: update load aws-documentation embedding to vector store

---
+++ settings.py
@@ -0,0 +1,7 @@
+"""
+Includes system settings
+"""
+
+COLLECTION_NAME = "knowledge_collection"
+MAX_VECTOR_DIMENSIONS = 512
+MODEL_ID = "mixedbread-ai/mxbai-embed-large-v1"
--- store.py
+++ store.py
@@ -3,33 +3,34 @@ read file knowledge.json, include list of metadata and vector
 and save vector to the milvus
 """
 
-from typing import Dict, List
-import json
+from typing import Dict, List, Any
+import pandas as pd
 
 from pymilvus import (
-    connections,
+    MilvusClient,
     FieldSchema,
     CollectionSchema,
     DataType,
-    Collection,
+    exceptions,
 )
+from settings import COLLECTION_NAME
 
 
 def create_collection(
+    _client: MilvusClient,
     name: str,
-    data: Dict[str, List[float]],
-) -> Collection:
+    dims: int,
+):
     """
     create milvus collection with name and data
     """
-    vector_size = len(list(data.values())[0])
+    vector_size = dims
     # create collection schema
     fields = [
         FieldSchema(
-            name="key",
+            name="id",
             dtype=DataType.INT64,
             is_primary=True,
-            auto_id=False,
         ),
         FieldSchema(
             name="embedding",
@@ -38,38 +39,117 @@ def create_collection(
         ),
         FieldSchema(
             name="document",
-            dtype=DataType.STRING,
+            dtype=DataType.VARCHAR,
+            max_length=65535,
         ),
     ]
-    schema = CollectionSchema(fields, "knowledge base")
-    collection = Collection(name, schema)
+    schema = CollectionSchema(
+        fields,
+        "knowledge base",
+        auto_id=True,
+        enable_dynamic_field=True,
+    )
+    _client.create_collection(
+        name,
+        schema=schema,
+    )
+    index_params = MilvusClient.prepare_index_params()
 
     # creat collection index
-    index = {
-        "index_type": "IVF_FLAT",
-        "metric_type": "L2",
-        "params": {"nlist": vector_size},
-    }
-    collection.create_index(field_name="embedding", index_params=index)
+    index_params.add_index(
+        field_name="embedding",
+        metric_type="COSINE",
+        index_type="IVF_FLAT",
+        index_name="embedding_index",
+        params={"nlist": vector_size},
+    )
+
+    _client.create_index(
+        collection_name=name,
+        index_params=index_params,
+    )
+
+
+def drop_collection(
+    _client: MilvusClient,
+    name: str,
+):
+    """
+    drop_collection drops collection with given name
+    """
+    try:
+        _client.drop_collection(collection_name=name)
+    except exceptions.MilvusException as e:
+        print("cannot drop collection", str(e))
+
+
+def reset_collection(
+    _client: MilvusClient,
+    name: str,
+):
+    """
+    reset_collection drop collection if exist,
+    """
+    try:
+        drop_collection(_client, name)
+    except exceptions.MilvusException as e:
+        print("collection not found", str(e))
+
+
+def insert_data(
+    _client: MilvusClient,
+    name: str,
+    data: Dict[str, List[float]],
+):
+    """
+    insert_data check if the collection is existed, if not create collection.
+    then insert data into collection
+    """
+    try:
+        _ = _client.get_collection_stats(name)
+    except exceptions.MilvusException:
+        vector_size = len(list(data.values())[0])
+        create_collection(_client, name, vector_size)
 
     # insert data to collection
     enities = [
-        list(range(len(data))),  # key
-        list(data.values()),  # embedding
-        list(data.keys()),  # document
+        {
+            "embedding": embedding,
+            "document": document,
+        }
+        for document, embedding in data.items()
     ]
-    collection.insert(enities)
+    try:
+        response: Dict[Any, Any] = _client.insert(
+            collection_name=name,
+            data=enities,
+        )
+    except exceptions.MilvusException as e:
+        print("cannot insert data", str(e))
+        print(data)
+        return
+    print(response, "inserted into vector db")
 
-    return collection
 
+pd.options.display.max_rows = 100
+pd.options.display.max_columns = 100
 
+pd.options.display.float_format = "{:,.2f}".format
 if __name__ == "__main__":
-    connections.connect(
-        "local",
-        host="localhost",
-        port="19530",
+    client = MilvusClient(uri="http://localhost:19530")
+
+    # read jsonl file in chunk with pd, each line of the jsonl is a dictionary
+    df = pd.read_json(
+        "aws_knowledge.jsonl",
+        lines=True,
+        chunksize=100,
+        typ="series",
     )
-    with open("knowledge.json", "r", encoding="utf-8") as file:
-        knowledge: Dict[str, List[float]] = json.load(file)
-        collection = create_collection("knowledge_collection", knowledge)
-        print("Done")
+
+    # loop in chunk, each line of the json is a dictionary
+    # (which is a dict with key is string and value is list of float)
+
+    for chunk in df:
+        for val in chunk.values:
+            knowledge: Dict[str, List[float]] = val
+            insert_data(client, COLLECTION_NAME, knowledge)
